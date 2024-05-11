5468c943e90f1922803420b072dcf9a1ffc141b9
Ignacio Diaz <informatica.diaz@gmail.com>
2024-05-11 11:35:13 -0300

    Style Cursiva

--- src/components/Cursiva.jsx
+++ src/components/Cursiva.jsx
@@ -7,21 +7,21 @@ export default function Cursiva() {
     setTextoConvertido(inputText)
   }
   return (
-    <div className='p-8'>
+    <div className='p-10'>
       <h1 className='font-cursiva text-8xl text-slate-400'>Letra Cursiva</h1>
-      <textarea rows="6" cols="50"
+      <textarea rows="3"
         className='border border-gray-300 rounded p-4 w-full'
         placeholder='Ingresa un texto'
         value={inputText}
         onChange={(e) => setInputText(e.target.value)}
       />
       <button
-        className='bg-slate-700 text-slate-400 font-bold p-4 rounded m-4'
+        className='bg-slate-700 text-slate-400 font-bold p-4 rounded mt-4'
         onClick={convertirACursiva}
       >
         Convertir a cursiva
       </button>
-      <p className='mt-4 font-cursiva text-6xl text-slate-400'>{textoConvertido}</p>
+      <p className='mt-4 font-cursiva text-9xl text-slate-400'>{textoConvertido}</p>
     </div>
   )
 }
