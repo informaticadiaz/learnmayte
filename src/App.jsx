70ea60513ec16092f67f8d338d9bcfa34496b78f
Ignacio Diaz <informatica.diaz@gmail.com>
2024-05-11 21:45:00 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <MainLayout>
     <div className='flex justify-center items-center h-screen bg-slate-900 text-slate-300'>
     <div className='text-center'>
-          <h1 className="max-w-6xl m-auto ">
+          <h1 className="m-auto text-5xl sm:text-7xl lg:text-9xl">
             Aprendiendo
           </h1>
         </div>
