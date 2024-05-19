b5fc13097ae727600b7f42313b67a365d222ff37
Ignacio Diaz <informatica.diaz@gmail.com>
2024-05-18 22:45:54 -0300

    config colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,18 @@ export default {
     extend: {
       fontFamily: {
         cursiva: ['cursiva', 'sans-serif'],
+      },
+      colors: {
+        'dark': '#001c32',
+        'bg-light': '#f9f9f9',
+        'card-dark': '#0d2b42',
+        'card-light': '#f9f9f9',
+        'btn-dark': '#345872',
+        'btn-light': '#f9f9f9',
+        't-btn-light': '#f9f9f9',
+        't-btn-dark': '#345872',
+        't-dark': '#6E85B2',
+        't-light': '#f9f9f9',
       }
     },
   },
