3286a111c9267c6cbc0df6a6a0a5711da0674f5d
Ignacio Diaz <informatica.diaz@gmail.com>
2024-05-21 10:45:47 -0300

    .

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5199072c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gq1tvtetfm"
+    "revision": "0.gvmf5inaao"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
