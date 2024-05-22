5bd6e36f67f274b8704cdde0bcab3d701ee7a475
Ignacio Diaz <informatica.diaz@gmail.com>
2024-05-22 18:43:20 -0300

    agrego a routes los componentes para la pagina matematicas

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,11 +1,18 @@
 import App from '../App';
+// pages
 import Matematica from '../pages/Matematica';
 import Lengua from '../pages/Lengua';
 import Sociales from '../pages/Sociales';
 import Naturales from '../pages/Naturales';
-import ErrorPage from '../error-page';
 import Tarea from '../pages/Tarea';
 import Dictionary from '../components/Dictionary';
+// components
+import SumarUnaCifra from '../components/matematica/sumar/SumarUnaCifra';
+import SumarDosCifras from '../components/matematica/sumar/SumarDosCifras';
+import SumarTresCifras from '../components/matematica/sumar/SumarTresCifras';
+// error
+import ErrorPage from '../error-page';
+
 
 const routes = [
   {
@@ -16,6 +23,18 @@ const routes = [
   {
     path: "/matematica",
     element: <Matematica />,
+  },
+  {
+    path: "/sumar/una-cifra",
+    element: <SumarUnaCifra />,
+  },
+  {
+    path: "/sumar/dos-cifras",
+    element: <SumarDosCifras />,
+  },
+  {
+    path: "/sumar/tres-cifras",
+    element: <SumarTresCifras />,
   },
   {
     path: "/lengua",
