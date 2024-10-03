bdff0e7488ee62424f1b8027734844c67e5c6d3c
Ignacio Diaz <informatica.diaz@gmail.com>
2024-10-03 10:14:09 -0300

    type=number"

--- src/components/matematica/sumar/SumarDosCifras.jsx
+++ src/components/matematica/sumar/SumarDosCifras.jsx
@@ -81,77 +81,80 @@ export default function SumarDosCifras() {
   return (
     <div>
       {mostrarCalculadora ? (
-    <div className="p-10 sm:p-20 bg-dark h-screen">
-      {/* suma */}
-      
-      <div className="grid grid-cols-3 gap-2 text-center text-4xl sm:text-7xl">
-        <div className="bg-btn-dark p-4 rounded col-span-1 row-span-2 flex flex-col justify-center">
-          <p className=" text-yellow-200">+</p>
-        </div>
-        <div className="bg-btn-dark p-4 rounded col-span-1">
-          <p>{d1}</p>
-        </div>
-        <div className="bg-btn-dark p-4 rounded col-span-1">
-          <p>{u1}</p>
-        </div>
-        <div className="bg-btn-dark p-4 rounded col-span-1 row-span-1">
-          <p>{d2}</p>
-        </div>
-        <div>
-          <div className="bg-btn-dark p-4 rounded col-span-1">
-            <p>{u2}</p>
+        <div className="p-10 sm:p-20 bg-dark h-screen">
+          {/* suma */}
+
+          <div className="grid grid-cols-3 gap-2 text-center text-4xl sm:text-7xl">
+            <div className="bg-btn-dark p-4 rounded col-span-1 row-span-2 flex flex-col justify-center">
+              <p className=" text-yellow-200">+</p>
+            </div>
+            <div className="bg-btn-dark p-4 rounded col-span-1">
+              <p>{d1}</p>
+            </div>
+            <div className="bg-btn-dark p-4 rounded col-span-1">
+              <p>{u1}</p>
+            </div>
+            <div className="bg-btn-dark p-4 rounded col-span-1 row-span-1">
+              <p>{d2}</p>
+            </div>
+            <div>
+              <div className="bg-btn-dark p-4 rounded col-span-1">
+                <p>{u2}</p>
+              </div>
+            </div>
           </div>
+          <hr className="my-2 border-yellow-200 border-2" />
+          {/* input respuesta */}
+          <form onSubmit={handleSubmit} >
+            <div className="grid grid-cols-3 gap-2 text-4xl text-center mt-2">
+              <div className="bg-btn-dark p-4 rounded col-span-1">
+                <input
+                  type="number"
+                  value={respuestaCentena}
+                  onChange={handleChangeCentenas}
+                  className="w-12 sm:w-20 sm:px-2 sm:py-4 sm:my-4 text-center"
+                />
+              </div>
+              <div className="bg-btn-dark p-4 rounded col-span-1">
+                <input
+                  type="number"
+                  value={respuestaDecenas}
+                  onChange={handleChangeDecenas}
+                  className="w-12 sm:w-20 sm:px-2 sm:py-4 sm:my-4 text-center"
+                />
+              </div>
+              <div className="bg-btn-dark p-4 rounded col-span-1">
+                <input
+                  type="number"
+                  value={respuestaUnidades}
+                  onChange={handleChangeUnidades}
+                  className="w-12 sm:w-20 sm:px-2 sm:py-4 sm:my-4 text-center"
+                />
+              </div>
+            </div>
+            <button
+              className="w-full m-auto bg-blue-800 text-slate-100 text-xl rounded p-2 mt-2"
+              type="submit"
+            >
+              CORREGIR
+            </button>
+          </form>
+
+          <p className="mt-4 text-t-btn-light text-4xl text-center">{mensaje}</p>
+          <p className="mt-4 text-t-btn-light text-4xl text-center">los aciertos son {aciertos}</p>
+          <p className="mt-4 text-t-btn-light text-4xl text-center">los errores son {errores}</p>
         </div>
-      </div>
-      <hr className="my-2 border-yellow-200 border-2" />
-      {/* input respuesta */}
-      <form onSubmit={handleSubmit} >
-        <div className="grid grid-cols-3 gap-2 text-4xl text-center mt-2">
-          <div className="bg-btn-dark p-4 rounded col-span-1">
-            <input
-              value={respuestaCentena}
-              onChange={handleChangeCentenas}
-              className="w-12 sm:w-20 sm:px-2 sm:py-4 sm:my-4 text-center"
-            />
-          </div>
-          <div className="bg-btn-dark p-4 rounded col-span-1">
-            <input
-              value={respuestaDecenas}
-              onChange={handleChangeDecenas}
-              className="w-12 sm:w-20 sm:px-2 sm:py-4 sm:my-4 text-center"
-            />
-          </div>
-          <div className="bg-btn-dark p-4 rounded col-span-1">
-            <input
-              value={respuestaUnidades}
-              onChange={handleChangeUnidades}
-              className="w-12 sm:w-20 sm:px-2 sm:py-4 sm:my-4 text-center"
-            />
-          </div>
+      ) : (
+        <div className="p-10 sm:p-20 bg-dark h-screen">
+          {/* Aquí va lo que se mostrará cuando la calculadora desaparezca */}
+          <h1 className="mt-4 text-t-btn-light text-7xl text-center">{mensaje}</h1>
+          <p className="mt-4 text-t-btn-light text-4xl text-center">los aciertos son {aciertos}</p>
+          <p className="mt-4 text-t-btn-light text-4xl text-center">los errores son {errores}</p>
+          <button
+            className="w-full m-auto bg-blue-800 text-slate-100 text-xl rounded p-2 mt-4"
+            onClick={() => window.location.reload()}>Reiniciar</button>
         </div>
-        <button
-          className="w-full m-auto bg-blue-800 text-slate-100 text-xl rounded p-2 mt-2"
-          type="submit"
-        >
-          CORREGIR
-        </button>
-      </form>
-      
-      <p className="mt-4 text-t-btn-light text-4xl text-center">{mensaje}</p>
-      <p className="mt-4 text-t-btn-light text-4xl text-center">los aciertos son {aciertos}</p>
-      <p className="mt-4 text-t-btn-light text-4xl text-center">los errores son {errores}</p>
-    </div>
-     ) : (
-      <div className="p-10 sm:p-20 bg-dark h-screen">
-        {/* Aquí va lo que se mostrará cuando la calculadora desaparezca */}
-        <h1 className="mt-4 text-t-btn-light text-7xl text-center">{mensaje}</h1>
-        <p className="mt-4 text-t-btn-light text-4xl text-center">los aciertos son {aciertos}</p>
-      <p className="mt-4 text-t-btn-light text-4xl text-center">los errores son {errores}</p>
-        <button
-        className="w-full m-auto bg-blue-800 text-slate-100 text-xl rounded p-2 mt-4"
-        onClick={() => window.location.reload()}>Reiniciar</button>
-      </div>
-    )}
+      )}
     </div>
   );
 }
