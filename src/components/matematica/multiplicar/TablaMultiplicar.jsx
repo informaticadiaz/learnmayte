1c0167c9dbac18d921d3aa308dea0543ef3b7b07
Ignacio Diaz <informatica.diaz@gmail.com>
2024-10-13 10:40:50 -0300

    se implementa conteo de aciertos sin sugerencias

--- src/components/matematica/multiplicar/TablaMultiplicar.jsx
+++ src/components/matematica/multiplicar/TablaMultiplicar.jsx
@@ -6,35 +6,33 @@ function TablaMultiplicar({ numeroTabla }) {
   const [respuesta, setRespuesta] = useState(''); // Para almacenar la respuesta del input
   const [mensaje, setMensaje] = useState(''); // Mensaje de corrección
   const [tablaResuelta, setTablaResuelta] = useState([]); // Almacena las respuestas correctas ya resueltas
+  const [aciertos, setAciertos] = useState(0); // Estado para contar los aciertos
+  const [sugerenciaMostrada, setSugerenciaMostrada] = useState(false); // Estado para verificar si se usó la sugerencia
 
-  // Resetea los estados cuando cambia la tabla de multiplicar
-  useEffect(() => {
-    setIndice(1);
-    setRespuesta('');
-    setMensaje('');
-    setTablaResuelta([]);
-  }, [numeroTabla]);
-
-  const scrollByPixels = (px) => {
-    window.scrollBy({
-      top: px,        // Cantidad de píxeles para desplazar verticalmente
-      behavior: 'smooth'  // Desplazamiento suave
-    });
-  };
 
   const corregirRespuesta = () => {
     const respuestaCorrecta = numeroTabla * indice;
     if (parseInt(respuesta) === respuestaCorrecta) {
+      // Si la respuesta es correcta y no se ha mostrado la sugerencia
+      if (!sugerenciaMostrada) {
+        setAciertos(aciertos + 1); // Aumentar el contador de aciertos
+      }
       // Si la respuesta es correcta, se almacena en la tabla resuelta
       setTablaResuelta([...tablaResuelta, `${numeroTabla} x ${indice} = ${respuestaCorrecta}`]);
       setIndice(indice + 1); // Avanzar al siguiente multiplicador
       setRespuesta(''); // Limpiar el input
       setMensaje('');
+      setSugerenciaMostrada(false); // Restablecer el estado de sugerencia
       // Desplazar hacia abajo 100px al corregir la respuesta
-    scrollByPixels(20);
+      scrollByPixels(20);
     } else {
       setMensaje('Incorrecto, intenta de nuevo');
     }
+  };
+
+  const darSugerencia = () => {
+    setMensaje(`La respuesta correcta es: ${numeroTabla * indice}`);
+    setSugerenciaMostrada(true); // Marcar que se ha mostrado la sugerencia
   };
 
   const handleKeyDown = (event) => {
@@ -43,16 +41,28 @@ function TablaMultiplicar({ numeroTabla }) {
     }
   };
 
-  const darSugerencia = () => {
-    setMensaje(`La respuesta correcta es: ${numeroTabla * indice}`);
-  };
-
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,           // Posición vertical a la que se desplaza (0 es la parte superior)
       behavior: 'smooth' // Desplazamiento suave
     });
   };
+
+  const scrollByPixels = (px) => {
+    window.scrollBy({
+      top: px,        // Cantidad de píxeles para desplazar verticalmente
+      behavior: 'smooth'  // Desplazamiento suave
+    });
+  };
+
+    // Resetea los estados cuando cambia la tabla de multiplicar
+    useEffect(() => {
+      setIndice(1);
+      setRespuesta('');
+      setMensaje('');
+      setTablaResuelta([]);
+      setAciertos(0);
+    }, [numeroTabla]);
 
   return (
     <div className="mt-8 bg-dark text-t-btn-light h-[130vh]">
@@ -94,7 +104,7 @@ function TablaMultiplicar({ numeroTabla }) {
             >
               Sugerir
             </button>
-            
+
             <button
               className="w-full m-auto bg-blue-800 text-slate-100 text-2xl rounded p-2 mt-2"
               onClick={corregirRespuesta}
@@ -109,16 +119,17 @@ function TablaMultiplicar({ numeroTabla }) {
       {/* Mensaje al finalizar la tabla */}
       {indice > 10 && (
         <>
-        <p className="text-lg sm:text-5xl text-center">¡Has completado la tabla del {numeroTabla}!</p>
-        <button
-              className="w-full m-auto bg-blue-800 text-slate-100 text-2xl rounded p-2 mt-2"
-              onClick={scrollToTop}
-            >
-              Tablas
-            </button>
+          <p className="text-lg sm:text-5xl text-center">¡Has completado la tabla del {numeroTabla}!</p>
+          <p className="text-lg sm:text-5xl text-center">Aciertos: {aciertos}</p>
+          <button
+            className="w-full m-auto bg-blue-800 text-slate-100 text-2xl rounded p-2 mt-2"
+            onClick={scrollToTop}
+          >
+            Tablas
+          </button>
         </>
-        
-      )      
+
+      )
       }
     </div>
   );
