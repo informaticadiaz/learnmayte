ac31ce4e8d7ca054f1a65593ab0716900aad6f46
Ignacio Diaz <informatica.diaz@gmail.com>
2024-10-20 19:37:40 -0300

    .

--- src/pages/Matematica.tsx
+++ src/pages/Matematica.tsx
@@ -106,14 +106,14 @@ export default function Matematica() {
             </button>
           </div>
         )}
-
+        {/* Submenú de Contar (puedes agregar más opciones aquí) */}
         {submenu === "contar" && (
           <div className="grid grid-cols-3 gap-4">
             <Link
               to="/incrementar/incremento"
               className="rounded bg-yellow-500 px-4 py-2 font-bold text-white hover:bg-yellow-700"
             >
-              Restar Una Cifra
+              Incremento
             </Link>
             
             <button
